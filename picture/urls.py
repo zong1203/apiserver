2cd8dd82dea01d5fcb69031964cc3ce7874aa346
zong1203 <zong90813@gmail.com>
2023-03-22 12:02:43 +0800

    把圖片上傳的部分寫好了(粗略)

--- picture/urls.py
+++ picture/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('get', views.get_image),
+    path('get/', views.get_image),
+    path('upload/', views.upload_image),
 ]
--- picture/views.py
+++ picture/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import os
+from django.http import HttpResponse,JsonResponse
+import os,json,base64
 # Create your views here.
 def get_image(request):
     try:
@@ -16,3 +16,16 @@ def get_image(request):
 def upload_image(request):
     if request.method == 'POST':
         body_unicode = request.body.decode('utf-8')
+        try:
+            body = json.loads(body_unicode)
+        except:
+            return JsonResponse({"fail":"please use json"})
+        if (body["type"] != "profile") and (body["type"] != "commodity"):
+            return HttpResponse("type error")
+        base64file = base64.b64decode(bytes(body["base64"], 'utf-8'))
+        img_file = open(f'./picture/{body["type"]}/image.jpg', 'wb')
+        img_file.write(base64file)
+        img_file.close()
+        return HttpResponse("ok")
+    return HttpResponse('some thing went wrong')
+    
